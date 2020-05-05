8d7aae0bdb28f9b0dfb5e94724a9f85d5c568354
felixlaarmann <felix.laarmann@tum.de>
2020-05-05 11:21:28 +0200

    Node Viz

--- cad_connection_test.m
+++ cad_connection_test.m
@@ -2,3 +2,27 @@
 
 path = 'C:\Users\Z420\Desktop\exchange_folder\F-BOM.xml';
 c=xml2struct(path); 
+
+
+%%
+
+
+
+%G = addnode(G,'asm')
+%G = addedge(G,'asm','part')
+
+
+
+
+G = digraph
+asmName = c.assembly.Attributes.name
+G = addnode(G,asmName)
+
+
+plot(G)
+
+
+function [res] = addNode()
+
+end
+
